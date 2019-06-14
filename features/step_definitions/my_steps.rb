e03c3d4bd73e01f80841c39b1463b5196dfe223b
Randy Eppinger <r@eppingers.name>
2019-06-14 16:38:45 -0400

    Scenario 2 complete

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -2,11 +2,18 @@ Given(/^I am on the home page$/) do
   @browser.goto 'http://localhost:3000'
 end
 
-
 When(/^I click on the puppy "([^"]*)"$/) do |name|
-  @browser.div(text: name).parent.button.click
+  @browser.div(text: name).parent.button(text: 'View Details').click
 end
 
 Then(/^I see "([^"]*)"$/) do |expected_text|
   expect(@browser.html.include?(expected_text)).to eq true
+end
+
+And(/^I click on "([^"]*)"$/) do |button_text|
+  @browser.button(text: button_text).click
+end
+
+Then(/^I see the home page$/) do
+  expect(@browser.h1(text: 'Puppy List')).to exist
 end
