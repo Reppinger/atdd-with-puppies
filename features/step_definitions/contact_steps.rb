b949cd0d682b5e5f77162343e484521284940530
Randy Eppinger <r@eppingers.name>
2019-06-17 15:01:38 -0400

    Contact Scenario #3 complete.

--- features/step_definitions/contact_steps.rb
+++ features/step_definitions/contact_steps.rb
@@ -2,9 +2,21 @@ When(/^I click on the email icon$/) do
   @browser.link(name: 'contact').click
 end
 
-And(/^I fill the form in with the following values:$/) do |form_values|
+When(/^I fill the form in with the following values:$/) do |form_values|
   contact = form_values.hashes.first
   @browser.text_field(id: 'contact_message_name').set(contact['name'])
   @browser.text_field(id: 'contact_message_email').set(contact['email'])
   @browser.textarea(id: 'contact_message_body').set(contact['message'])
+end
+
+When(/^the contact form is completely filled in$/) do
+  steps %Q{
+      And I fill the form in with the following values:
+      | name        | email           | message                                            |
+      | Joe Sixpack | joe@sixpack.com | I am having trouble adopting a puppy. Please help! |
+  }
+end
+
+When(/^I blank out "([^"]*)"$/) do |element_id|
+  @browser.element(id: element_id).wd.clear
 end
